d0231a513f4efc43cbc3a23d1a0a2aa7d14f2a7e
W0lffi <forenaddress@gmx.de>
2024-10-14 22:09:04 +0200

    feat: add pause cue method and logging

--- src/main/java/org/wolffi/cueprogram/controller/PlayerController.java
+++ src/main/java/org/wolffi/cueprogram/controller/PlayerController.java
@@ -1,13 +1,20 @@
 package org.wolffi.cueprogram.controller;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.wolffi.cueprogram.model.AudioCue;
 import org.wolffi.cueprogram.model.Cue;
 import org.wolffi.cueprogram.model.Player;
 import org.wolffi.cueprogram.view.PlayerView;
 
 
+/**
+ * ToDo: Generic generation of cues so cue type hasn't to be specified here
+ */
 public class PlayerController {
+
+    private static final Logger log = LogManager.getLogger();
 
     private final Player model;
     private final PlayerView view;
@@ -16,13 +23,17 @@ public class PlayerController {
         this.model = model;
         this.view = view;
         this.view.getBtnAddCue().setOnAction(_ -> addCue("Mond mit der Hand berührt", "/mnt/shared_data/Musik/(Disc 5) 05 - Hab Den Mond Mit Der Hand Berührt.mp3", 100.0d));
-        this.view.getBtnPlayCue().setOnAction(_ -> playCue(this.view.getCueView().getSelectionModel().getSelectedIndex()));
-        this.view.getBtnStopCue().setOnAction(_ -> stopCue(this.view.getCueView().getSelectionModel().getSelectedIndex()));
-        this.view.getBtnFadeOutCue().setOnAction(_ -> fadeOutCue(this.view.getCueView().getSelectionModel().getSelectedIndex()));
+        this.view.getBtnPlayCue().setOnAction(_ -> playCue(this.view.getSelectedCueIndex()));
+        this.view.getBtnStopCue().setOnAction(_ -> stopCue(this.view.getSelectedCueIndex()));
+        this.view.getBtnFadeOutCue().setOnAction(_ -> fadeOutCue(this.view.getSelectedCueIndex()));
+        this.view.getBtnPauseCue().setOnAction(_ -> pauseCue(this.view.getSelectedCueIndex()));
     }
 
     private void addCue(String name, String path, double volume) {
+        log.info("Cue will be added to list");
+        log.debug("With following parameters: name={}, path={}, volume={}", name, path, volume);
         if (StringUtils.isEmpty(name)) {
+            log.debug("Using path as name because no name is provided");
             name = path;
         }
         Cue cue = new AudioCue(name, path, volume);
@@ -32,23 +43,30 @@ public class PlayerController {
     private boolean isValidIndex(int index) { return index >= 0; }
 
     private void playCue(int index) {
-        System.out.println(index);
+        log.info("Start to play cue with following index: {}", index);
         if (isValidIndex(index)) {
             this.model.playCue(index);
         }
     }
 
     private void stopCue(int index) {
-        System.out.println(index);
+        log.info("Stop cue with index: {}", index);
         if (isValidIndex(index)) {
             this.model.stopCue(index);
         }
     }
 
-    private void fadeOutCue(int index) {
-        System.out.println(index);
+    private void pauseCue(int index) {
+        log.info("Pause cue with index: {}", index);
         if (isValidIndex(index)) {
-            this.model.fadeOutCue(index, 3000.0d);
+            this.model.pauseCue(index);
+        }
+    }
+
+    private void fadeOutCue(int index) {
+        log.info("Fade out cue with index: {}", index);
+        if (isValidIndex(index)) {
+            this.model.fadeOutCue(index);
         }
     }
 }
