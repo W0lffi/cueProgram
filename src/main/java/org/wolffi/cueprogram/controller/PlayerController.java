92e0083d4c908f634f0635ad03f9557b2262b1bb
W0lffi <forenaddress@gmx.de>
2024-10-18 17:18:47 +0200

    refactor: change log level from debug to info and add list index to CueFactory call

--- src/main/java/org/wolffi/cueprogram/controller/PlayerController.java
+++ src/main/java/org/wolffi/cueprogram/controller/PlayerController.java
@@ -17,44 +17,44 @@ public class PlayerController {
 
     public PlayerController(Player model, PlayerView view) {
         this.model = model;
-        view.getBtnAddCue().setOnAction(_ -> addCue("Mond mit der Hand berührt", "/mnt/shared_data/Musik/(Disc 5) 05 - Hab Den Mond Mit Der Hand Berührt.mp3", 100.0d));
+        view.getBtnAddCue().setOnAction(_ -> addCue("Mond mit der Hand berührt", "/mnt/shared_data/Musik/(Disc 5) 05 - Hab Den Mond Mit Der Hand Berührt.mp3", 100.0d, view.getCueListSize()));
         view.getBtnPlayCue().setOnAction(_ -> playCue(view.getSelectedCueIndex()));
         view.getBtnStopCue().setOnAction(_ -> stopCue(view.getSelectedCueIndex()));
         view.getBtnFadeOutCue().setOnAction(_ -> fadeOutCue(view.getSelectedCueIndex()));
         view.getBtnPauseCue().setOnAction(_ -> pauseCue(view.getSelectedCueIndex()));
     }
 
-    private void addCue(String name, String path, double volume) {
-        log.info("Cue will be added to list");
-        Cue cue = CueFactory.createCue(CueType.AUDIO, name, path, volume);
+    private void addCue(String name, String path, double volume, int listIndex) {
+        log.debug("Will add cue to list");
+        Cue cue = CueFactory.createCue(CueType.AUDIO, listIndex, name, path, volume);
         model.addCue(cue);
     }
 
     private boolean isValidIndex(int index) { return index >= 0; }
 
     private void playCue(int index) {
-        log.info("Start to play cue with following index: {}", index);
+        log.debug("Will play cue with index: {}", index);
         if (isValidIndex(index)) {
             this.model.playCue(index);
         }
     }
 
     private void stopCue(int index) {
-        log.info("Stop cue with index: {}", index);
+        log.debug("Will stop cue with index: {}", index);
         if (isValidIndex(index)) {
             this.model.stopCue(index);
         }
     }
 
     private void pauseCue(int index) {
-        log.info("Pause cue with index: {}", index);
+        log.debug("Will pause cue with index: {}", index);
         if (isValidIndex(index)) {
             this.model.pauseCue(index);
         }
     }
 
     private void fadeOutCue(int index) {
-        log.info("Fade out cue with index: {}", index);
+        log.debug("Will fade out cue with index: {}", index);
         if (isValidIndex(index)) {
             this.model.fadeOutCue(index);
         }
