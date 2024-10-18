880f6428c2a5ac1565ee839ae29d650ee9561a52
W0lffi <forenaddress@gmx.de>
2024-10-18 17:15:55 +0200

    feat: add setter for name and complete getter of it

--- src/main/java/org/wolffi/cueprogram/model/cue/DescriptionCue.java
+++ src/main/java/org/wolffi/cueprogram/model/cue/DescriptionCue.java
@@ -3,27 +3,24 @@ package org.wolffi.cueprogram.model.cue;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class DescriptionCue implements Cue{
+public class DescriptionCue implements Cue {
 
     private static final Logger log = LogManager.getLogger();
-    private final String name;
+
+    private String name;
 
     public DescriptionCue(String name) {
         this.name = name;
     }
 
     @Override
-    public void setStart(double startTime) {
-
-    }
-
-    @Override
-    public void setStop(double endTime) {
-
+    public void setName(String name) {
+        log.debug("Change description cue name from {} to {}", this.name, name);
+        this.name = name;
     }
 
     @Override
     public String getName() {
-        return "";
+        return this.name;
     }
 }
