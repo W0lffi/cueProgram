c2946c4807da12d0ecd7f0341656df955838c674
W0lffi <forenaddress@gmx.de>
2024-10-18 17:15:01 +0200

    fix: AudioCue constructor call and log message

--- src/main/java/org/wolffi/cueprogram/model/cue/CueFactory.java
+++ src/main/java/org/wolffi/cueprogram/model/cue/CueFactory.java
@@ -19,12 +19,12 @@ public final class CueFactory {
     public static Cue createCue(CueType type, Object ...params) {
         return switch (type) {
             case AUDIO -> {
-                log.debug("With following parameters: name={}, path={}, volume={}", params[0], params[1], params[2]);
-                if (StringUtils.isEmpty((String) params[0])) {
+                log.debug("With following parameters: name={}, path={}, volume={}", params[1], params[2], params[3]);
+                if (StringUtils.isEmpty((String) params[1])) {
                     log.debug("Using path as name because no name is provided");
-                    params[0] = params[1];
+                    params[1] = params[2];
                 }
-                yield new AudioCue((String) params[0], (String) params[1], (double) params[2]);
+                yield new AudioCue((int) params[0], (String) params[1], (String) params[2], (double) params[3]);
             }
             case DESCRIPTION -> {
                 log.debug("With following parameters: name={}", params[0]);
